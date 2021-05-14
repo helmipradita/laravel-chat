970cdec82c0dcb22d709da515120250f2888b05f
Helmi Pradita <mr.hellmii@gmail.com>
2021-05-15 00:55:08 +0700

    setting css lalu compile dengan npm run dev

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Chat</title>
+
+    <link rel="stylesheet" href="./css/app.css">
 </head>
 <body>
-    <h1>Hello, World!</h1>
+    <div class="app">
+        <header>
+            <h1>Let's Talk!</h1>
+            <input type="text" name="username" id="username" placeholder="Please enter a username...">
+        </header>
+
+        <div id="messages"></div>
+        
+        <form id="message_form">
+            <input type="text" name="message" id="message_input" placeholder="Write a message....">
+            <button type="submit" id="message_send">Send Massage</button>
+        </form>
+    </div>
+
+    <script src="./js/app.js"></script>
 </body>
 </html>
